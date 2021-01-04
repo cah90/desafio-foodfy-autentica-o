ab2745f69804288d095c2f68e0c9da4989b2f829
Cássia <cascrisbern@gmail.com>
2021-01-04 18:41:18 -0300

    adding the user to the database creating a random password to be inserted into the database making a hash of this password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,6 @@
 const db = require("../../config/db")
+const generatePassword = require("password-generator")
+const {hash} = require("bcryptjs")
 
 module.exports = {
   async findOne(email) {
@@ -9,5 +11,44 @@ module.exports = {
     `
     const results = await db.query(query, [email])
     return results.rows[0]
+  }, 
+
+  async create(data) {
+    const query = `
+      INSERT INTO users (
+        name,
+        email,
+        password
+      ) VALUES ($1, $2, $3)
+      RETURNING id
+    `
+    //CREATE A PASSWORD
+     const password = generatePassword(8,false) //create in another module utils
+
+    //HASH OF PASSWORD
+    const passwordHash = await hash(password, 8)
+
+    const values = [
+      data.name,
+      data.email,
+      passwordHash
+    ]
+
+    let results
+
+    try {
+      results = await db.query(query, values)
+    } catch(err) {
+      console.log("This is an errror when inserting users.")
+      console.error(err)
+    }
+    
+    //SEND THE PASSWORD TO THE USER
+
+
+    if(results) {
+      return results.rows[0].id
+    }
   }
 }
+
